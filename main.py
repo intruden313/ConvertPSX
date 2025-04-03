6fe9bfc606f152e1be9198c4503af1e12f53f96b
intruden313 <mattpistritto@gmail.com>
2025-04-02 22:33:20 -0400

    Successfully converting files; Next: Delete Old

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 import subprocess
-import os
+from pathlib import Path
 
-path = input("Please Provide the folder where your .chd files are")
+def find_CHD(rompath): 
+    """
+    This function simply scans a directory and sub-directories and identifies all .chd files
 
+    Returns:
+    None
+    """
+    chd_files = [] #Initialize an Empty list
 
-def convertchdtocue(path,filetype):
-    subprocess.run(f'chdman extractcd -i "{path}.{filetype}" -o "{path}.cue" --force', shell=True)
+    #Iterates through all directories and sub directories looking for .chd files
+    for file_path in rompath.rglob("*.chd"):
+        if file_path.is_file():
+            chd_files.append(file_path) #appends each file path to the list         
+    return chd_files
+
+def convert_chd_to_cue(chd_file):
+    cue_file = chd_file.with_suffix('.cue') #takes the name
+    subprocess.run(f'chdman extractcd -i "{chd_file}" -o "{cue_file}" --force', shell=True)
+    print(f"Converted {chd_file} to {cue_file}")
+
+if __name__ == "__main__":
+    rominput = input("Paste Your PSX ROM directory: ")
+    rompath = Path(rominput) #Necessary to define the input string as a "Path" variable
+    chd_list = find_CHD(rompath) #Runs the find_CHD function and assigns the output list to chd_list
+    
+    if not chd_list:
+        print("No CHD files found")
+    else:
+        for gamefiles in chd_list:
+            convert_chd_to_cue(gamefiles)
