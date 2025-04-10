da3b861483b259c49ae96866a2c9ee04e73bc130
intruden313 <mattpistritto@gmail.com>
2025-04-10 19:41:45 -0400

    Trying to Open a file browser

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import subprocess
+import os, platform
+import platform
 from pathlib import Path
+
+
+def openOSRespectiveFM(directory: Path) -> None:
+    def run_windows() -> bool:
+        return os.name in ['nt', 'ce']
+    def run_macOS() -> bool:
+        return "darwin" in platform.system().casefold()
+    
+    if run_windows():
+        os.startfile(os.path.normpath(directory))
+    elif run_macOS:
+        subprocess.run(['open',str(directory)], check = True)
+    else:
+        subprocess.run(['xdg-open', str(directory)], check = True)
+    
+
+
 
 def find_CHD(rompath): 
     """
@@ -24,7 +43,9 @@ def convert_chd_to_cue(chd_file):
 
 # Execute Code
 if __name__ == "__main__":
-    rominput = input("Paste Your PSX ROM directory: ")
+    #rominput = input("Paste Your PSX ROM directory: ")
+    print(f'Please Select a Path: ')
+    openOSRespectiveFM()
     rompath = Path(rominput) #Necessary to define the input string as a "Path" variable
     chd_list = find_CHD(rompath) #Runs the find_CHD function and assigns the output list to chd_list
     
